0ba7ddc7dfdf014c42222968fd06da0f11e8b824
Daniel Biskup <1309272+DanielBiskup@users.noreply.github.com>
2021-08-10 05:43:30 +0200

    Split Character off into it's own file

--- script.js
+++ script.js
@@ -214,64 +214,6 @@ class AnimatedSprite {
   }
 }
 
-class Character {
-  constructor(canvas, renderer) {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 3.5 + 1.5;
-    const characterActions = ['up', 'right', 'down right'];
-    this.action =
-      characterActions[
-        Math.floor(Math.random() * characterActions.length)
-      ];
-    this.animatedSprite = new AnimatedSprite(
-      images.player,
-      animationData,
-      renderer
-    );
-    this.animatedSprite.startAnimation(this.action);
-    this.width = this.animatedSprite.frameWidth;
-    this.height = this.animatedSprite.frameHeight;
-  }
-
-  draw() {
-    this.animatedSprite.draw(this.x, this.y);
-  }
-
-  update(dtsec) {
-    // Update Components:
-    this.animatedSprite.update(dtsec);
-
-    // Logic:
-    if (this.action === 'right') {
-      if (this.x > canvas.width) {
-        this.x = 0 - this.width;
-        this.y =
-          Math.random() * canvas.height - this.height;
-      } else {
-        this.x += this.speed;
-      }
-    }
-    if (this.action === 'up') {
-      if (this.y < 0 - this.height) {
-        this.y = canvas.height;
-        this.x = Math.random() * canvas.width;
-      } else {
-        this.y -= this.speed;
-      }
-    }
-    if (this.action == 'down right') {
-      if (this.y > canvas.height || this.x > canvas.width) {
-        this.y = 0 - this.height;
-        this.x = Math.random() * canvas.width;
-      } else {
-        this.x += this.speed;
-        this.y += this.speed;
-      }
-    }
-  }
-}
-
 function drawBackground() {
   let backgroundPattern = ctx.createPattern(
     images.background,
